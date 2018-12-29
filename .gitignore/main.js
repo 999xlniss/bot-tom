e096749ebf94aa4bc25654a6f7c65481c700637c
999xlniss <46231677+999xlniss@users.noreply.github.com>
2018-12-29 22:55:32 +0100

    Create main.js

---
+++ .gitignore/main.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefixe = ('/');
+
+client.login('NTI4NjM5NDAwMzY4NjY4Njc2.DwlNiA.143VVHUw63vOAyRzzcvIZa-xjpc');
+
+client.on('message', msg => {
+    if (msg.content === prefixe + 'help')
+  msg.channel.sendMessage('/help pour voir ce menu \n !play + url ou nom de musique pour lancer un son \n !leave pour enlever le bot ryhtm \n !stop pour mettre en pause la musique \n pour collab ou autre allez voir secu ou alnis ');
+    console.log('aide demandée');
+});
+
+client.on('message', msg => {
+    if (msg.content === prefixe + 'salut tom')
+    msg.channel.sendMessage('Salut , un service ?');
+    console.log('un fdp a parlé')
+});
+
+client.on('message', msg => {
+    if (msg.content === ('ping'))
+    msg.channel.sendMessage('Pong !');
+    console.log('Ping Pong !')
+});
+
+client.on('message', msg => {
+    if (msg.content === ('qui est lpb ?'))
+    msg.channel.sendMessage('Alnis bien sûr');
+    console.log('Alnis lpb')
+});
+
+client.on('message', msg => {
+    if (msg.content === ('qui est la plus moche ?'))
+    msg.channel.sendMessage('christiane sans hésiter');
+    console.log('christiane la moche')
+});
